40cc30dfdf14e90aee782acb44b2a6a6c2fe7702
William Getz <willegetz@gmail.com>
2017-12-18 07:32:11 -0800

    Rollback to working state.

--- MusicalDiceTests/MusicalDiceTest.cs
+++ MusicalDiceTests/MusicalDiceTest.cs
@@ -105,59 +105,6 @@ G5 2 1";
             Assert.AreEqual(expected, actual);
         }
 
-        [TestMethod]
-        public void FirstMeasureBeatsFromMeasure22Ordered0To3()
-        {
-            var firstMeasureRaw = RetrieveBeatsForMeasure(22);
-
-            var actual = OrderBeatsForNewMeasurePlacement(1, firstMeasureRaw);
-
-            var expected = @"C3 0 2
-E5 0 1
-C5 1 1
-G4 2 1";
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void FirstMeasureBeatsFromMeasure176Ordered0To3()
-        {
-            var firstMeasureRaw = RetrieveBeatsForMeasure(176);
-
-            var actual = OrderBeatsForNewMeasurePlacement(1, firstMeasureRaw);
-
-            var expected = @"A5 0 0.5
-B2 0 2
-D3 0 2
-G5 0.5 0.5
-B5 1 0.5
-G5 1.5 0.5
-B2 2 1
-D3 2 1
-D5 2 0.5
-G5 2.5 0.5";
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void SecondMeasureBeatsFromMeasure1Ordered3To6()
-        {
-            var secondMeasureRaw = RetrieveBeatsForMeasure(1);
-
-            var actual = OrderBeatsForNewMeasurePlacement(2, secondMeasureRaw);
-
-            var expected = @"F3 3 1
-F5 3 1
-D3 4 1
-D5 4 1
-G3 5 1
-G5 5 1";
-
-            Assert.AreEqual(expected, actual);
-        }
-
         private string RetrieveBeatsForMeasure(int measure)
         {
             var measureLength = 3;
@@ -178,44 +125,6 @@ G5 5 1";
                 {
                     sb.Append(currentMeasure).Append("\n");
                 }
-            }
-
-            return sb.ToString().Trim();
-        }
-
-        private string OrderBeatsForNewMeasurePlacement(int desiredMeasureNumber, string firstMeasureRaw)
-        {
-            var sb = new StringBuilder();
-
-            var measureLength = 3;
-            var firstBeat = (desiredMeasureNumber - 1) * measureLength;
-            var lastBeat = firstBeat + measureLength;
-
-            var newBeatOrder = new[] { firstBeat, firstBeat++, lastBeat };
-
-            var measureArray = firstMeasureRaw.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-
-            var firstOfMeasuresParts = measureArray[0].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            var measureOffset = double.Parse(firstOfMeasuresParts[1]);
-
-            foreach (var measure in measureArray)
-            {
-                var currentMeasureParts = measure.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-                // Get a the beat number, like 2 or 3.5
-                var currentMeasureNumber = double.Parse(currentMeasureParts[1]);
-
-                // Assign the new beat number, like 6 or 9.5
-
-
-
-                var beatIndex = currentMeasureNumber - measureOffset;
-
-                
-                currentMeasureParts[1] = newBeatOrder[0].ToString();
-
-                var newMeasure = string.Join(" ", currentMeasureParts);
-                sb.Append(newMeasure).Append("\n");
             }
 
             return sb.ToString().Trim();
