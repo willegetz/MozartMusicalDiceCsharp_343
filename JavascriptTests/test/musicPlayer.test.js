0f30e5266c45f34431f85ab5babbe2afe9019b3d
William Getz <willegetz@gmail.com>
2018-01-05 08:10:17 -0800

    Initial javascript unit testing commit.

---
+++ JavascriptTests/test/musicPlayer.test.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const player = require('../../MusicalDicePlayer/Scripts/Custom/musicPlayer');
+
+describe('Music Player', function(){
+  it('should parse a beat into three parts', function(){
+    assert.fail('Not implemented yet.');
+  })
+})
+
