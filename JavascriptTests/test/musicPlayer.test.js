ba5d544ca2d1efd9dfd8cb0d0d6e973c002e44e6
William Getz <willegetz@gmail.com>
2018-01-10 08:12:54 -0800

    Can get desired note frequency with its octave.

--- JavascriptTests/test/musicPlayer.test.js
+++ JavascriptTests/test/musicPlayer.test.js
@@ -1,17 +1,29 @@
 'Use Strict'
 
 const assert = require('assert');
-const musicPlayer = require('../../MusicalDicePlayer/Scripts/Custom/musicPlayer');
+const musicPlayer = require('../../MusicalDicePlayer/Scripts/Custom/musicPlayer').exportedFunctions;
 
 describe('Musica Player', function () {
   console.log(JSON.stringify(musicPlayer, null, 4));
 
   describe('cosmologicon\'s note / octave dictionary', function () {
     it('should provide a note name to index map', function () {
-      let notenames = musicPlayer.exportedFunctions.buildNoteIndex();
+      let notenames = musicPlayer.buildNoteIndex();
       let expectedNotenames = require('./approvedFiles/cosmologiconNoteNames.json');
 
       assert.deepEqual(notenames, expectedNotenames);
     })
-  })
+  });
+
+  it('should calculate the octave frequency in hz for note C3 to be 261.63hz', function () {
+    let notenames = musicPlayer.buildNoteIndex();
+
+    const middleC = 'C4';
+    const octaveOfMiddleC = notenames[middleC];
+
+    let middleCFrequency = musicPlayer.calculateFrequencyForNote(octaveOfMiddleC);
+
+    const expectedFrequency = 261.63;
+    assert.equal(middleCFrequency, expectedFrequency);
+  });
 });
--- MusicalDicePlayer/Scripts/Custom/musicPlayer.js
+++ MusicalDicePlayer/Scripts/Custom/musicPlayer.js
@@ -1,7 +1,7 @@
 ﻿'use strict';
 
 const exportedFunctions = {
-    buildNoteIndex: function() {
+    buildNoteIndex: function () {
         let notenames = {};
         "C C# D D# E F F# G G# A A# B".split(" ").forEach((n, j) => {
             for (let octave = -1; octave < 8; ++octave) {
@@ -10,6 +10,19 @@ const exportedFunctions = {
         });
 
         return notenames;
+    },
+    calculateFrequencyForNote: function (octaveOfNote) {
+        const frequencyOfAnchorNoteA4 = 440;
+        const octaveOfAnchorNoteA4 = 69;
+
+        const semitoneFrequencyRatio = Math.pow(2, (1 / 12));
+
+        const stepsFromAnchorNote = octaveOfNote - octaveOfAnchorNoteA4;
+
+        const newNoteFrequency = frequencyOfAnchorNoteA4 * Math.pow(semitoneFrequencyRatio, stepsFromAnchorNote);
+        const roundedFrequency = Math.round(newNoteFrequency * 100) / 100;
+
+        return roundedFrequency;
     },
 }
 
