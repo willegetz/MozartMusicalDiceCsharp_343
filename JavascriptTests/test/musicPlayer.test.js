71081927004c69678a825d6b44529c8c96dbd850
William Getz <willegetz@gmail.com>
2018-01-24 20:16:13 -0800

    Fixed spelling errors. Added test for start and stop time of a note

--- JavascriptTests/test/musicPlayer.test.js
+++ JavascriptTests/test/musicPlayer.test.js
@@ -3,7 +3,7 @@
 const assert = require('assert');
 const musicPlayer = require('../../MusicalDicePlayer/Scripts/Custom/musicPlayer').exportedFunctions;
 
-describe('Musica Player', function () {
+describe('Musical Player', function () {
   console.log(JSON.stringify(musicPlayer, null, 4));
 
   describe('cosmologicon\'s note / octave dictionary', function () {
@@ -15,7 +15,7 @@ describe('Musica Player', function () {
     })
   });
 
-  it('should calculate the octave frequency in hz for note C3 to be 261.63hz', function () {
+  it('should calculate the octave frequency in hz for note C4 to be 261.63hz', function () {
     let notenames = musicPlayer.buildNoteIndex();
 
     const middleC = 'C4';
@@ -25,5 +25,25 @@ describe('Musica Player', function () {
 
     const expectedFrequency = 261.63;
     assert.equal(middleCFrequency, expectedFrequency);
+  });
+
+  it('should calculate the start and stop time of a note given a tempo of 120, a starting beat of 3, and a duration of 2 beats', function () {
+    const tempo = 120;
+    const minuteAsSeconds = 60;
+
+    const startBeat = 3;
+    const duration = 2;
+
+    let beatToTime = minuteAsSeconds / tempo;
+
+    let startingTime = startBeat * beatToTime;
+    let durationTime = duration * beatToTime;
+    let stoppingTime = startingTime + durationTime;
+
+    const expectedStartTime = 1.5;
+    const expectedStopTime = 2.5;
+
+    assert.equal(startingTime, expectedStartTime);
+    assert.equal(stoppingTime, expectedStopTime);
   });
 });
