bf0434844d05c84feed4442c4712087300ab68f0
William Getz <willegetz@gmail.com>
2018-01-24 21:53:32 -0800

    refactoring and clean up

--- JavascriptTests/test/musicPlayer.test.js
+++ JavascriptTests/test/musicPlayer.test.js
@@ -4,22 +4,23 @@ const assert = require('assert');
 const musicPlayer = require('../../MusicalDicePlayer/Scripts/Custom/musicPlayer').exportedFunctions;
 
 describe('Musical Player', function () {
-  console.log(JSON.stringify(musicPlayer, null, 4));
+  let noteNames = {};
+
+  beforeEach(function () {
+    noteNames = musicPlayer.buildNoteIndex();
+  });
 
   describe('cosmologicon\'s note / octave dictionary', function () {
     it('should provide a note name to index map', function () {
-      let notenames = musicPlayer.buildNoteIndex();
-      let expectedNotenames = require('./approvedFiles/cosmologiconNoteNames.json');
+      let expectedNoteNames = require('./approvedFiles/cosmologiconNoteNames.json');
 
-      assert.deepEqual(notenames, expectedNotenames);
+      assert.deepEqual(noteNames, expectedNoteNames);
     })
   });
 
   it('should calculate the octave frequency in hz for note C4 to be 261.63hz', function () {
-    let notenames = musicPlayer.buildNoteIndex();
-
     const middleC = 'C4';
-    const octaveOfMiddleC = notenames[middleC];
+    const octaveOfMiddleC = noteNames[middleC];
 
     let middleCFrequency = musicPlayer.calculateFrequencyForNote(octaveOfMiddleC);
 
@@ -28,22 +29,15 @@ describe('Musical Player', function () {
   });
 
   it('should calculate the start and stop time of a note given a tempo of 120, a starting beat of 3, and a duration of 2 beats', function () {
-    const tempo = 120;
-    const minuteAsSeconds = 60;
-
     const startBeat = 3;
     const duration = 2;
 
-    let beatToTime = minuteAsSeconds / tempo;
-
-    let startingTime = startBeat * beatToTime;
-    let durationTime = duration * beatToTime;
-    let stoppingTime = startingTime + durationTime;
+    let noteTiming = musicPlayer.getNoteStartAndStopTimes(startBeat, duration);
 
     const expectedStartTime = 1.5;
     const expectedStopTime = 2.5;
 
-    assert.equal(startingTime, expectedStartTime);
-    assert.equal(stoppingTime, expectedStopTime);
+    assert.equal(noteTiming.start, expectedStartTime);
+    assert.equal(noteTiming.stop, expectedStopTime);
   });
 });
--- MusicalDicePlayer/Scripts/Custom/musicPlayer.js
+++ MusicalDicePlayer/Scripts/Custom/musicPlayer.js
@@ -1,4 +1,9 @@
 ﻿'use strict';
+const frequencyOfAnchorNoteA4 = 440;
+const minuteAsSeconds = 60;
+const octaveOfAnchorNoteA4 = 69;
+const semitoneFrequencyRatio = Math.pow(2, (1 / 12));
+const tempo = 120;
 
 const exportedFunctions = {
     buildNoteIndex: function () {
@@ -12,17 +17,19 @@ const exportedFunctions = {
         return notenames;
     },
     calculateFrequencyForNote: function (octaveOfNote) {
-        const frequencyOfAnchorNoteA4 = 440;
-        const octaveOfAnchorNoteA4 = 69;
-
-        const semitoneFrequencyRatio = Math.pow(2, (1 / 12));
-
         const stepsFromAnchorNote = octaveOfNote - octaveOfAnchorNoteA4;
 
         const newNoteFrequency = frequencyOfAnchorNoteA4 * Math.pow(semitoneFrequencyRatio, stepsFromAnchorNote);
         const roundedFrequency = Math.round(newNoteFrequency * 100) / 100;
 
         return roundedFrequency;
+    },
+    getNoteStartAndStopTimes: function (startBeat, durationBeats) {
+        let start = startBeat * minuteAsSeconds / tempo;
+        let duration = durationBeats * minuteAsSeconds / tempo;
+        let stop = start + duration;
+
+        return { start: start, stop: stop };
     },
 }
 
